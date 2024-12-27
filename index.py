81d549d73a8684630692d1d3ed2eea21c968efdb
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-12-27 17:44:26 +0900

    playwright set up complete

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print('hello')
+# fetch information using playwright
+# URL: https://www.gamesindustry.biz/events?year=2025
+# parse information to only get required informaition for this current month
+
+
+# PARSE DATA TO CREATE A SPREADSHEET FILE
+
+# init function to run everything
+
+def init():
+    print('test')
+
+
+init()  
