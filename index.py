a0dd52e2a0ed2ea3056aa6700925d6816bf6a80a
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-12-27 20:28:29 +0900

    decided to just get all motnhs instead of the current

--- index.py
+++ index.py
@@ -1,14 +1,72 @@
-# fetch information using playwright
-# URL: https://www.gamesindustry.biz/events?year=2025
-# parse information to only get required informaition for this current month
+from datetime import datetime
+from playwright.sync_api import sync_playwright
+# /////////////------THIS ALLOWS US TO GET BY THE CURRENT ITS ONLY HALF FINISH ---------//////
+# def scrape_by_month():
+#     with sync_playwright() as playwright:
+#         # Launch a browser (headless by default)
+#         browser = playwright.chromium.launch(headless=True)
+#         # Open a new browser context and page
+#         context = browser.new_context()
+#         page = context.new_page()
+#         # TODO; WILLH HAVE TO UPDATE THIS AFTER DECEMBER ENDS
+#         current_year = datetime.now().year + 1
+#         current_month = ' January'
+#         # Navigate to the website
+#         page.goto("https://www.gamesindustry.biz/events?year=2025")
+#         # have to target month class and get all children buy 
+#         article_parent_element = page.locator('div.months')
+#         article_month_header = article_parent_element.locator('div.month :first-child')
+#         print(article_month_header.text_content())
+        
+#         # check if the current year matches and month
+#         # if current year adn month matches then we continue
+#         # if str(current_year + ) in article_header:
+#         #     print("Year Matches")
+#         #     # get access to element by class and then get children
+#         #     eventsParentElement = page.locator('')
+#         #     return 
+#         # return print("Did not match")
+#         # Extract and print the page title
+#         # title = page.title()
+
+#         # print(f"Page title: {title}")
+
+#         # Close the browser
+#         browser.close()
+# /////////////------THIS ALLOWS US TO GET BY THE CURRENT ITS ONLY HALF FINISH ---------//////
+def scrape_months():
+    with sync_playwright() as playwright:
+        # Launch a browser (headless by default)
+        browser = playwright.chromium.launch(headless=True)
+        # Open a new browser context and page
+        context = browser.new_context()
+        page = context.new_page()
+        # TODO; WILLH HAVE TO UPDATE THIS AFTER DECEMBER ENDS
+        current_year = datetime.now().year + 1
+        current_month = ' January'
+        # Navigate to the website
+        page.goto("https://www.gamesindustry.biz/events?year=2025")
+        # have to target month class and get all children buy 
+        article_parent_element = page.locator('div.months')
+        article_month_header = article_parent_element.locator('div.month :first-child')
+        # print(article_month_header.text_content())
+        
+        # check if the current year matches and month
+        # if current year adn month matches then we continue
+        # if str(current_year + ) in article_header:
+        #     print("Year Matches")
+        #     # get access to element by class and then get children
+        #     eventsParentElement = page.locator('')
+        #     return 
+        # return print("Did not match")
+        # Extract and print the page title
+        # title = page.title()
+
+        # print(f"Page title: {title}")
+
+        # Close the browser
+        browser.close()
 
 
-# PARSE DATA TO CREATE A SPREADSHEET FILE
-
-# init function to run everything
-
-def init():
-    print('test')
-
-
-init()  
+if __name__ == "__main__":
+    # scrape_title()
